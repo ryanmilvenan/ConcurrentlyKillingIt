229cb3c7d4cd50def170e4ad52f96f88a59bc8e3
Ryan Milvenan <ryanmilvenan@gmail.com>
2014-11-03 13:12:47 -0700

    Added clojurescript pathing

--- muzak/project.clj
+++ muzak/project.clj
@@ -4,8 +4,17 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.2.0"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [enfocus "2.1.0"]]
+  :plugins [[lein-ring "0.8.13"]
+            [lein-cljsbuild "1.0.3"]]
+  :cljsbuild {
+    :builds [{
+      :sourth-paths ["src-cljs"]
+      :compiler {
+        :output-to "resources/public/js/main.js"
+        :optimizations :whitespace
+        :pretty-print true}}]}
   :ring {:handler muzak.core.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
