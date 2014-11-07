1638991b281f60f7b4802dc766c2af3e051f8b2e
David Ellis <ellisda@gmail.com>
2014-11-07 13:46:33 -0700

    add a couple extra routes - ex: Curl API
    Here is an example of how to parse a numeric value from the Client URL "request map".
    
    When we have Clojure code to serve up HDF5 results, we can put that code behind routes that parse the user's request (Curl API request)

--- muzak/src/muzak/core/handler.clj
+++ muzak/src/muzak/core/handler.clj
@@ -3,8 +3,15 @@
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
 
+; parse-int - Clojure code to parse a string as an integer
+(defn parse-int [s]
+  (Integer. (re-find  #"\d+" s )))
+
 (defroutes app-routes
   (GET "/" [] "Hello World!")
+  (GET "/user/:id" [id] (str "<h1>Hello user " (+ 10 (parse-int id)) " (we added 10) </h1>"))
+  (GET "/info" {ip :remote-addr} (str "<h1>Hello client IP " ip "  </h1>"))
+  (GET "/requestmap" request (str request))
   (route/not-found "Not Found"))
 
 (def app
