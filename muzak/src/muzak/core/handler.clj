f4f4f7d9bde0baa1e9601e6a2a552bdab3c80ade
joshkillinger <josh.killinger@gmail.com>
2014-11-21 16:47:53 -0700

    Added dynamic attribute parsing
    Uses a map of location-attribute pairs to make a map of attribute-value
    pairs.

--- muzak/src/muzak/core/handler.clj
+++ muzak/src/muzak/core/handler.clj
@@ -40,17 +40,21 @@
   (def wconfig (. ch.systemsx.cisd.hdf5.HDF5Factory configure "attribute.h5"))
   (def writer (.writer wconfig))
   (.close writer))
-  
+
 (defn hdf5-get-reader []
   (. ch.systemsx.cisd.hdf5.HDF5Factory openForReading "resources/TRAXLZU12903D05F94.h5"))
 
-(defn hdf5-get-attribute []
+(defn hdf5-get-compound [hr] [x]
+  {(:attr x) (.getMemberInfo hr (:attr x))})
+
+;builds a map of attribute-value pairs
+(defn hdf5-get-attribute [x]
   (def hr (hdf5-get-reader))
-  (.getStringAttribute hr "/metadata/artist_terms" "TITLE"))
-  
+  {(:attr x) (.getStringAttribute hr (:path x) (:attr x))})
+
 (defn hdf5-getStringHdf5Reader [hr]
   (.string hr))
-  
+
 (defn hdf5-readStringArray [hr]
   (def sr (hdf5-getStringHdf5Reader hr))
   (.readArray sr "/metadata/similar_artists"))
@@ -58,12 +62,32 @@
 ; NOTE: I believe that the Song_Title is part of a compound data set, needing a compound reader
 (defn hdf5-getCompoundHdf5Reader [hr]
   (.compound hr))
-  
+
+;(defn hdf5-get-attr [x]
+;  (def hr (hdf5-get-reader))
+;  (.getStringAttribute hr (get x :path) (get x :attr)))
+
+;attempt at reading an arbitrary list of tags
+;(def x [{:path "/metadata/atrist_terms" :attr "TITLE"}])
+(defn get-song-details [x]
+  (def hr (hdf5-get-reader))
+  (mapcat (partial hdf5-get-compound hr) x))
+
+;test with testParams
+(defn get-song [x]
+  (mapcat hdf5-get-attribute x))
+
+;use to test get-song (does not work, needs compound reader)
+(def testParams [{:path "/metadata/songs" :attr "artist_name"} {:path "/metadata/songs" :attr "title"}])
+;use to test get-song (works)
+(def testOriginal [{:path "/metadata/artist_terms" :attr "title"}])
+
+
 ;(defn hdf5-getCompoundValue [hr]
 ;  (def cr (hdf5-getCompoundHdf5Reader hr))
   ; FAIL - We don't know what type of Java object to ask for (ref: http://svncisd.ethz.ch/doc/hdf5/hdf5-13.06/
 ;  )
-  
+
 
 ;; Event listeners
 (defn ws-handler [{:keys [ws-channel] :as req}]
