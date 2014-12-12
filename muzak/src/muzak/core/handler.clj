3a8f497c20c12520526aac0e2e5a86af1db165fe
M-S-F <msfuhrman@gmail.com>
2014-12-11 21:17:26 -0700

    Passes string to magic-write-edn

--- muzak/src/muzak/core/handler.clj
+++ muzak/src/muzak/core/handler.clj
@@ -40,9 +40,10 @@
 (defn parse-event-handler [ws-channel addr data error]
   (update-search-map addr data)
   ;; NEED Mangle client request into single like "rock"
+  (def keywords (clojure.string/lower-case (apply name (filter (merge-with not= data default-map) (keys data)))))
   ;;    TODO - Should be easy to extend HDF5 filter to work with collection of string terms
   ;; something like ("rock" "pop" "metal")
-  (magic-write-edn data)
+  (magic-write-edn keywords)
   (>!! ws-channel (if error
                     (format "Error: '%s'." (pr-str data))
                     (hash-map :event "result" :data data))))
